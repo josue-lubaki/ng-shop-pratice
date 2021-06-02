44ccd9ca8118d65bd7b150a7eb70eb8319c6ecd2
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-02 05:50:08 -0400

    create first API & parsing JSON Data

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get('/', (req, res) => {
-    res.send("hello API")
+// npm install dotenv
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+// Middleware
+app.use(express.json())
+
+// http://localhost:3000/api/v1/products
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some url',
+    }
+    res.send(product)
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct)
+    res.send(newProduct)
 })
 
 app.listen(3000, () => {
+    console.log(api)
     console.log('server is running now : http://localhost:3000')
 })
