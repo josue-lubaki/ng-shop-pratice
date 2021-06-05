65a82da08f8bdbab4b8ab56b79fc404080eeef08
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-05 01:46:03 -0400

    CRD for product

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -2,6 +2,9 @@ const { Category } = require('../models/category')
 const express = require('express')
 const router = express.Router()
 
+/**
+ * Récuperation des caterogies contenues dans la collection Categories
+ */
 router.get(`/`, async (req, res) => {
     const categoryList = await Category.find()
 
@@ -11,6 +14,53 @@ router.get(`/`, async (req, res) => {
         })
     }
     res.send(categoryList)
+})
+
+/**
+ * Création d'une nouvelle Category dans la collection catégories
+ */
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+    })
+
+    category = await category.save()
+
+    if (!category) {
+        return res.status(404).send('the category cannot be created')
+    }
+
+    res.send(category)
+})
+
+/**
+ * Suppression d'une category dans la collection Categories
+ * @see findByIdAndDelete
+ * @see /api/v1/:id
+ */
+router.delete('/:id', async (req, res) => {
+    Category.findByIdAndDelete(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'The category is deleted',
+                })
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'category not found !',
+                })
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({
+                success: false,
+                error: err,
+            })
+        })
 })
 
 module.exports = router
