9b459ca312ab996497ddaa5f64c39897e06ac337
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-05 02:52:20 -0400

    Post a new Product REST API

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,4 +1,5 @@
 const { Product } = require('../models/product')
+const { Category } = require('../models/category')
 const express = require('express')
 const router = express.Router()
 
@@ -14,24 +15,39 @@ router.get(`/`, async (req, res) => {
     res.send(productList)
 })
 
-router.post(`/`, (req, res) => {
-    const product = new Product({
+/**
+ * Création d'un produit dans la collection Product
+ * @method findById()
+ * @see http://localhost:3000/api/v1/products
+ */
+router.post(`/`, async (req, res) => {
+    // Vérifier l'existence de la catgorie avant de créer un produit
+    const category = await Category.findById(req.body.category)
+    if (!category) {
+        return res.status(400).send('invalid Category')
+    }
+
+    let product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
         countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
 
-    product
-        .save()
-        .then((createdProduct) => {
-            res.status(201).json(createdProduct)
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false,
-            })
-        })
+    product = await product.save()
+
+    if (!product) {
+        return res.status(500).send('The product cannot be craeted')
+    }
+
+    res.send(product)
 })
 
 module.exports = router
