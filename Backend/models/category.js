a165a1addb80eefd271a7a8007898f4a8d5a77d3
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-05 01:20:17 -0400

    Schema complet Product et Category

--- Backend/models/category.js
+++ Backend/models/category.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose')
 
 // Le Schema du model
-const categorySchema = mongoose.Schema({})
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    color: {
+        type: String,
+    },
+})
 
 exports.Category = mongoose.model('Category', categorySchema)
