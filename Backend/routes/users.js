a2c20d15e686dcc4226296c88d9d0d0b78f7d4cd
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-06 05:23:47 -0400

    Login a User REST API & Creating a Token

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,6 +2,7 @@ const { User } = require('../models/user')
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 /**
  * Récupération de tous les Utilisateurs
@@ -65,6 +66,9 @@ router.post(`/`, async (req, res) => {
 
 /**
  * Mise à jour des informations de l'Utilisateur
+ * @method findById()
+ * @method bcrypt.hashSync(text, salt)
+ * @see http://localhost:3000/api/v1/users/[:id]
  */
 router.put('/:id', async (req, res) => {
     // Vérifier l'existence de l'Utilisateur
@@ -96,6 +100,36 @@ router.put('/:id', async (req, res) => {
         return res.status(400).send('The user cannot be update !')
     }
     res.send(user)
+})
+
+/**
+ * Methode qui permet de logger un Utilisateur
+ * @method findOne()
+ * @method jwt.sign(base_du_token, mot_secret, delai_de_vie_du_token)
+ */
+router.post('/login', async (req, res) => {
+    // Vérifier si l'Utilisateur existe vai son email
+    const user = await User.findOne({ email: req.body.email })
+    const secret = process.env.secret
+    if (!user) {
+        return res.status(400).send('The user not found')
+    }
+
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id,
+            },
+            secret,
+            { expiresIn: '1d' }
+        )
+        res.status(200).send({
+            user: user.email,
+            token: token,
+        })
+    } else {
+        res.status(400).send('Password is wrong')
+    }
 })
 
 module.exports = router
