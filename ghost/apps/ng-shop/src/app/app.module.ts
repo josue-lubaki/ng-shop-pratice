ab5b4359fe112c96af9d539f4d75ac4881258cb8
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-07 22:41:06 -0400

    add two components HomePage and Product-List

--- ghost/apps/ng-shop/src/app/app.module.ts
+++ ghost/apps/ng-shop/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
