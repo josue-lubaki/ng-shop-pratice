f9a1d364f03cfeefc5330a703f5e76b2f9fcee19
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-08 01:24:05 -0400

    Creating Components Inside Libraries and Use them in the Apps

--- ghost/apps/ng-shop/src/app/app.module.ts
+++ ghost/apps/ng-shop/src/app/app.module.ts
@@ -7,17 +7,17 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@ghost/ui';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'products', component: ProductListComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
-
