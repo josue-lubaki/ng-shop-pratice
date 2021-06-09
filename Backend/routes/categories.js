35031be3915d794544d217f22218b5501271a2d9
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-09 02:07:21 -0400

    fix bug importing moongose #OK

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -1,6 +1,7 @@
 const { Category } = require('../models/category')
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 /**
  * Récuperation des caterogies contenues dans la collection Categories
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
 /**
  * Récupération de tous les Utilisateurs
