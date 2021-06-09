c75cebf1d9bfc769e4b2bca5728f8b2c1b78eaf9
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-09 14:07:57 -0400

    Editor for Product Detailed Description

--- ghost/apps/admin/src/app/app.module.ts
+++ ghost/apps/admin/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { DropdownModule } from 'primeng/dropdown';
+import { EditorModule } from 'primeng/editor';
 
 const UX_MODULE = [
     CardModule,
@@ -41,7 +42,8 @@ const UX_MODULE = [
     InputNumberModule,
     InputTextareaModule,
     InputSwitchModule,
-    DropdownModule
+    DropdownModule,
+    EditorModule
 ];
 
 const routes: Routes = [
