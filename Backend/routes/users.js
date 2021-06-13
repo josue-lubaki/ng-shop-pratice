aa006a3daae15f39526ac05dc795c1b29903ef0f
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-13 14:37:04 -0400

    fix bug password

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -47,7 +47,7 @@ router.post(`/`, async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
