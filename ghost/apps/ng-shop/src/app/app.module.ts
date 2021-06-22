43aa8dd3a9efbd0b58fc93304e47207e13025d47
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-06-22 01:40:42 -0400

    categories banner

--- ghost/apps/ng-shop/src/app/app.module.ts
+++ ghost/apps/ng-shop/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './shared/nav/nav.component';
 import { FormBuilder } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
     { path: '', component: HomePageComponent },
@@ -33,6 +34,7 @@ const routes: Routes = [
         BrowserModule,
         RouterModule.forRoot(routes),
         AccordionModule,
+        HttpClientModule,
         BrowserAnimationsModule,
         ProductsModule,
         UiModule
